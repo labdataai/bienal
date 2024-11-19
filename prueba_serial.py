87e80f7b3072e8e4aa2b1e4c33bcf1503975c578
MarceBenitez <cmarcelobenitez@gmail.com>
2024-11-19 15:15:04 -0300

    prueba puerto serie

---
+++ prueba_serial.py
@@ -0,0 +1,9 @@
+#https://github.com/MrYsLab/microbit-robot/blob/master/remote_controllers/gui_controllers/tkinter/tk_controller.py
+
+#device="MMI0"
+
+import serial
+
+serial_port = serial.Serial(port = "ttyAMA0", baudrate = 9600)
+serial_port.readline()
+
